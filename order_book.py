cff9ed20c885438c4ba460bcea02ec5cc4896e3e
lechu91 <51946227+lechu91@users.noreply.github.com>
2022-07-25 01:03:40 -0700

    Update order_book.py

--- order_book.py
+++ order_book.py
@@ -44,13 +44,15 @@ def process_order(order):
             buy_amount = existing_order.buy_amount - new_order.sell_amount
             sell_amount = existing_order.sell_amount / existing_order.buy_amount * buy_amount
             
-            child_order = {'buy_currency': existing_order.buy_currency,
+            child_data = {'buy_currency': existing_order.buy_currency,
                            'sell_currency': existing_order.sell_currency,
                            'buy_amount': buy_amount,
                            'sell_amount': sell_amount,
                            'sender_pk': existing_order.sender_pk,
                            'receiver_pk': existing_order.receiver_pk
                           }
+            
+            child_order = Order(**{f:child_data[f] for f in fields})
             
             session.add(child_order)
             session.commit()
