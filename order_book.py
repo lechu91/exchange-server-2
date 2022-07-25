2cb484dbb6439af7d3d66e325e131785de58d976
lechu91 <51946227+lechu91@users.noreply.github.com>
2022-07-25 01:22:17 -0700

    Update order_book.py

--- order_book.py
+++ order_book.py
@@ -8,14 +8,19 @@ Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
-fields = ['sender_pk','receiver_pk','buy_currency','sell_currency','buy_amount','sell_amount','creator_id']
+fields_basic = ['sender_pk','receiver_pk','buy_currency','sell_currency','buy_amount','sell_amount']
+field_child = ['sender_pk','receiver_pk','buy_currency','sell_currency','buy_amount','sell_amount','creator_id']
 
-def process_order(order):
+def process_order(order, child=False):
     #Your code here
     
     #Insert order into data base
-    
-    new_order = Order(**{f:order[f] for f in fields})
+
+    if child = False:
+        new_order = Order(**{f:order[f] for f in fields_basic})
+    else:
+        new_order = Order(**{f:order[f] for f in fields_child})
+            
     session.add(new_order)
     session.commit()
     
@@ -26,13 +31,9 @@ def process_order(order):
         
         # Check if currencies match
         if existing_order.buy_currency == new_order.sell_currency and existing_order.sell_currency == new_order.buy_currency:
-            
-            print("orders with same currency found")
-           
+
             # Check if exchange rates match
             if existing_order.sell_amount * new_order.sell_amount >= new_order.buy_amount * existing_order.buy_amount:
-                
-                print("orders with compatible exchange rate found")
                 
                 #If a match is found between order and existing_order do the trade
                 existing_order.filled = datetime.now()
@@ -41,8 +42,6 @@ def process_order(order):
                 new_order.counterparty_id = existing_order.id
                 session.commit()
                 break
-                
-            print("orders didn't have a compatible exchange rate")
                     
     if existing_order.buy_amount > new_order.sell_amount:
         #create order
